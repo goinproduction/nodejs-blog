8b8c1f39ba79dbde1ec2716816e5d3b46e2fd161
viincyy <viincyy126@gmail.com>
2021-08-05 17:21:29 +0700

    routes && controller

--- src/index.js
+++ src/index.js
@@ -5,10 +5,19 @@ const handlebars = require("express-handlebars");
 const app = express();
 const port = 3000;
 
+const route = require("./routes");
+
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.json());
+
 //HTTP logger
-app.use(morgan("combined"));
+// app.use(morgan("combined"));
 
 // template engine
 app.engine(
@@ -20,13 +29,8 @@ app.engine(
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "/resources/views"));
 
-app.get("/", (req, res) => {
-  res.render("home");
-});
-
-app.get("/news", (req, res) => {
-  res.render("news");
-});
+// Route initialize
+route(app);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
