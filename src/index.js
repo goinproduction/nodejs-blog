9e59f46971ba712476d92c392a743c304fe5d2ad
viincyy <viincyy126@gmail.com>
2021-08-07 12:55:06 +0700

    sort courses

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const morgan = require('morgan');
 const handlebars = require('express-handlebars');
 const methodOverride = require('method-override');
+const SortMiddleware = require('./app/middlewares/SortMiddleware.js');
 const app = express();
 const port = 3000;
 
@@ -23,6 +24,7 @@ app.use(express.json());
 
 app.use(methodOverride('_method'));
 
+app.use(SortMiddleware);
 //HTTP logger
 // app.use(morgan("combined"));
 
@@ -33,6 +35,28 @@ app.engine(
         extname: '.hbs',
         helpers: {
             sum: (a, b) => a + b,
+            sortable: (field, sort) => {
+                const sortType = field === sort.column ? sort.type : 'default';
+                const icons = {
+                    default: 'bi bi-filter-circle-fill',
+                    asc: 'bi bi-sort-down-alt',
+                    desc: 'bi bi-sort-up-alt',
+                };
+
+                const types = {
+                    default: 'desc',
+                    asc: 'desc',
+                    desc: 'asc',
+                };
+
+                const icon = icons[sortType];
+                const type = types[sortType];
+                return `
+                    <a href="?_sort&column=${field}&type=${type}">
+                        <i class="${icon}"></i>
+                    </a>
+                `;
+            },
         },
     }),
 );
